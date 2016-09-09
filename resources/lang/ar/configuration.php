3e2931aae6209970e17d2af63d0affb2a5c1e608
aladinfajr <aladinfajr@gmail.com>
2016-09-09 11:50:53 +0300

    translate to Arabic language

---
+++ resources/lang/ar/configuration.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'operator' => [
+        'name'    => 'المعامل',
+        'options' => [
+            'has'           => 'يملك',
+            'does_not_have' => 'لا يملك',
+        ],
+    ],
+    'value'    => [
+        'name' => 'القيمة',
+    ],
+];
---
+++ resources/lang/ar/message.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'filter' => 'المنتج <em>:operator</em> <strong>:value</strong> وسم',
+];
